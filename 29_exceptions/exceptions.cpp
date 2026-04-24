b6a508168ca8264a8becf621c195d912142b605c
edeberry7355 <edeberry7355@floridapoly.edu>
2026-04-24 13:51:03 +0000

    Add exceptions 2

--- 29_exceptions/exceptions.cpp
+++ 29_exceptions/exceptions.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include <stdexcept>
+
+// Can create our own exceptions
+class MyException : std::logic_error {
+public:
+    MyException(int code, const std::string& msg)
+        : std::logic_error(msg), err_code(code) {} // calling the constructor of the base class
+
+    int getCode() const {
+        return err_code;
+    }
+
+    const char* what() const noexcept override {
+        std::string out("The exception is ");
+        out += std::to_string(getCode());
+        // std::cout << "The exception code is " << getCode() << std::endl;
+        out += std::logic_error::what();
+        //std::cout << std::logic_error::what() << std::endl;
+        
+        return out.c_str();
+    }
+
+private:
+    int err_code;
+};
+
 
 class Fraction {
 public:
@@ -8,9 +34,17 @@ public:
 
     int quotient() {
         if (den == 0) {
-            throw "Cannot divide by zero"; // this is a character array, not a string
+            // throw "Cannot divide by zero"; // this is a character array, not a string
+            throw std::runtime_error("Cannot divide by zero");
         }
-        throw 1;
+        // If the fraction is negative throw logic_error
+        if ((num < 0 && den > 0) || (num > 0 && den < 0)) {
+            throw std::logic_error("The result is negative");
+        }
+        // if quotient greater than 100 throw MyException with code 13
+        if (num / den > 100) {
+            throw MyException(13, "The quotient is greater than 100");
+        }
 
         return num / den;
     }
@@ -30,8 +64,19 @@ int main(void) {
         std::cout << f1.quotient() << std::endl;
         std::cout << "End of try block\n";
     }
-    catch (const char* ex) { // type of catch is the type of ...
-        std::cout << "Char* exception was caught: " << ex << std::endl;
+    // pass catch by constant reference
+    catch (const MyException& ex) {
+        std::cout << "MyException exception was caught: " << ex.what() << std::endl;
+    }
+    // catch (const char* ex) { // type of catch is the type of ...
+    catch (const std::logic_error& ex) {
+        // std::cout << "Char* exception was caught: " << ex << std::endl;
+        std::cout << "Logic_error exception was caught: " << ex.what() << std::endl; // what is defined in base class and overriden in all child classes 
+    }
+    catch (const std::exception& ex) {
+        // Runtime is a kid of exception
+        // This block must be after logic error because more specific goes first, more broad goes after
+        std::cout << "Base class exception was caught: " << ex.what() << std::endl; // what is defined in base class and overriden in all child classes 
     }
 
     std::cout << "End of program\n";
--- notes/18_chapter_exception_handling.md
+++ notes/18_chapter_exception_handling.md
@@ -32,3 +32,54 @@ catch -> fixes or reports
 * For dealing with unexpected error conditions. Do not use a try-catch block to deal with simple, expected situtations
 * If you want the exception to be processed by it's caller, you should throw it
 * If you can handle the exception in the function where it occurs, there is no need to throw or use exceptions
+
+## Review
+
+If I'm a function and I throw an exception, the caller of the function will try to handle the exception.
+
+    try {
+        code 1; // can throw an int
+        code 2;
+    }
+    catch (int ex) {
+        code 3;
+    }
+    code 4;
+
+* If no exceptions are thrown, code 1, code 2, and code 4 will be executed
+* If code 1 throws an exception, we go to code 3 then go to code 4
+* If a double was thrown, the code would not work
+
+## Practice
+
+* Input 120, throws exception, catches, then continues
+* Input 80, no throw no catch, continue
+
+## Multiple Catches
+
+* Can have multiple catch blocks after try to be able to catch different types of functions
+* `catch (...)` - can catch anything, any type
+* Catch blocks need to be directly after the try block
+
+## Exception Classes
+
+* All live in the header `<stdexcept>`
+
+## Custom Exception Classes
+
+* Can create your own exception class
+* Just like any ...
+
+## Catch block
+
+* Various
+* The order
+* Must check the grandkid before the kid and the kid before the parent
+
+## Exception Propagation
+
+* ...
+
+### Example
+
+If an exception is thrown in function3
